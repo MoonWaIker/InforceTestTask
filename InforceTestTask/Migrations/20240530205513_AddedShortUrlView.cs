5ae922fc83b6fa27d420c4a0c45682b62124136a
Michaerl <muhinmihajlo40@gmail.com>
2024-05-31 14:48:30 +0300

    Added migrations for ShortUrl

---
+++ InforceTestTask/Migrations/20240530205513_AddedShortUrlView.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InforceTestTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedShortUrlView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShortUrlViews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatorId = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortUrlViews", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortUrlViews");
+        }
+    }
+}
---
+++ InforceTestTask/Migrations/20240530210017_UpdatedNameOfShortUrls.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InforceTestTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedNameOfShortUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShortUrlViews",
+                table: "ShortUrlViews");
+
+            migrationBuilder.RenameTable(
+                name: "ShortUrlViews",
+                newName: "ShortUrl");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShortUrl",
+                table: "ShortUrl",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShortUrl",
+                table: "ShortUrl");
+
+            migrationBuilder.RenameTable(
+                name: "ShortUrl",
+                newName: "ShortUrlViews");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShortUrlViews",
+                table: "ShortUrlViews",
+                column: "Id");
+        }
+    }
+}
