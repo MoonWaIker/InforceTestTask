8f087ee36b0b43d91a3cc874e60c4a825e03e43d
Michael <muhinmihajlo40@gmail.com>
2025-06-25 19:28:03 +0300

    Refactored the ShortenUrl record

--- InforceTestTask.DataBase/Entities/ShortenUrl.cs
+++ InforceTestTask.DataBase/Entities/ShortenUrl.cs
@@ -1,23 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
 using Microsoft.EntityFrameworkCore;
 
 namespace InforceTestTask.DataBase.Entities;
 
 [PrimaryKey(nameof(Id))]
 [Index(nameof(OriginalUrl), IsUnique = true)]
-[Index(nameof(ShortenedUrl), IsUnique = true)]
+[Index(nameof(UrlHash), IsUnique = true)]
 public record ShortenUrl
 {
-    public int Id { get; init; }
+    public BigInteger Id { get; init; }
 
     [Required]
     public required Uri OriginalUrl { get; init; }
 
     [Required]
-    public required Uri ShortenedUrl { get; init; }
+    public required int UrlHash { get; init; }
 
-    public DateTime CreatedDate { get; init; } = DateTimeOffset.UtcNow.UtcDateTime;
+    public DateTime CreatedDate { get; init; }
 
     public long UserId { get; init; }
 
