51ebf083fba96fdc5afdf6820e143dda04c4ef23
Michael <muhinmihajlo40@gmail.com>
2025-06-25 19:26:36 +0300

    Refactored the ShortenUrlController to use service

--- InforceTestTask/Controllers/ShortenUrlController.cs
+++ InforceTestTask/Controllers/ShortenUrlController.cs
@@ -1,108 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using InforceTestTask.DataBase.Contexts;
 using InforceTestTask.DataBase.Entities;
+using InforceTestTask.Infrastructure.Interfaces;
 
-namespace InforceTestTask.Controllers
+namespace InforceTestTask.Controllers;
+
+[Route(ApiRoute)]
+[ApiController]
+public class ShortenUrlController(IShortUrlService shortUrlService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ShortenUrlController : ControllerBase
+    private const string IdRoute = "{id:BigInteger}";
+    private const string ApiRoute = "api/[controller]";
+
+    [HttpGet]
+    public ActionResult<IEnumerable<ShortenUrl>> GetShortenUrls()
     {
-        private readonly InforceDbContext _context;
+        return Ok(shortUrlService.GetShortenUrls);
+    }
 
-        public ShortenUrlController(InforceDbContext context)
-        {
-            _context = context;
-        }
+    [HttpGet(IdRoute)]
+    public ActionResult<ShortenUrl> GetShortenUrl(int id)
+    {
+        return shortUrlService.GetShortenUrl(id);
+    }
 
-        // GET: api/ShortenUrl
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ShortenUrl>>> GetShortenUrls()
-        {
-            return await _context.ShortenUrls.ToListAsync();
-        }
+    [HttpPut]
+    public IActionResult PutShortenUrl(ShortenUrl shortenUrl)
+    {
+        shortUrlService.UpdateShortenUrl(shortenUrl);
 
-        // GET: api/ShortenUrl/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ShortenUrl>> GetShortenUrl(int id)
-        {
-            var shortenUrl = await _context.ShortenUrls.FindAsync(id);
+        return NoContent();
+    }
 
-            if (shortenUrl == null)
-            {
-                return NotFound();
-            }
+    [HttpPost]
+    public ActionResult<ShortenUrl> PostShortenUrl(ShortenUrl shortenUrl)
+    {
+        shortUrlService.AddShortenUrl(shortenUrl);
 
-            return shortenUrl;
-        }
+        return Created();
+    }
 
-        // PUT: api/ShortenUrl/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutShortenUrl(int id, ShortenUrl shortenUrl)
-        {
-            if (id != shortenUrl.Id)
-            {
-                return BadRequest();
-            }
+    [HttpDelete(IdRoute)]
+    public IActionResult DeleteShortenUrl(int id)
+    {
+        shortUrlService.DeleteShortenUrl(id);
 
-            _context.Entry(shortenUrl).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ShortenUrlExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/ShortenUrl
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<ShortenUrl>> PostShortenUrl(ShortenUrl shortenUrl)
-        {
-            _context.ShortenUrls.Add(shortenUrl);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetShortenUrl", new { id = shortenUrl.Id }, shortenUrl);
-        }
-
-        // DELETE: api/ShortenUrl/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteShortenUrl(int id)
-        {
-            var shortenUrl = await _context.ShortenUrls.FindAsync(id);
-            if (shortenUrl == null)
-            {
-                return NotFound();
-            }
-
-            _context.ShortenUrls.Remove(shortenUrl);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ShortenUrlExists(int id)
-        {
-            return _context.ShortenUrls.Any(e => e.Id == id);
-        }
+        return NoContent();
     }
 }
