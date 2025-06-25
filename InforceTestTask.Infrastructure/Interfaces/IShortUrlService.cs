55b1007ae88d03309e92c0327720a15955661bce
Michael <muhinmihajlo40@gmail.com>
2025-06-25 19:27:35 +0300

    Refactored the ShortUrlService

--- InforceTestTask.Infrastructure/Interfaces/IShortUrlService.cs
+++ InforceTestTask.Infrastructure/Interfaces/IShortUrlService.cs
@@ -1,6 +1,16 @@
+using InforceTestTask.DataBase.Entities;
+
 namespace InforceTestTask.Infrastructure.Interfaces;
 
 public interface IShortUrlService
 {
+    IEnumerable<ShortenUrl> GetShortenUrls { get; }
 
+    ShortenUrl GetShortenUrl(int id);
+
+    void UpdateShortenUrl(ShortenUrl shortenUrl);
+
+    void AddShortenUrl(ShortenUrl shortenUrl);
+
+    void DeleteShortenUrl(int id);
 }
--- InforceTestTask.Infrastructure/Services/ShortUrlService.cs
+++ InforceTestTask.Infrastructure/Services/ShortUrlService.cs
@@ -1,8 +1,39 @@
-﻿using InforceTestTask.Infrastructure.Interfaces;
+﻿using InforceTestTask.DataBase.Contexts;
+using InforceTestTask.DataBase.Entities;
+using InforceTestTask.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace InforceTestTask.Infrastructure.Services;
 
-public class ShortUrlService : IShortUrlService
+public class ShortUrlService(InforceDbContext context) : IShortUrlService
 {
+    public IEnumerable<ShortenUrl> GetShortenUrls => context.ShortenUrls;
 
+    public ShortenUrl GetShortenUrl(int id)
+    {
+        return context.ShortenUrls
+        .Include(url => url.User)
+        .First(url => url.Id == id);
+    }
+
+    public void UpdateShortenUrl(ShortenUrl shortenUrl)
+    {
+        context.Update(shortenUrl);
+
+        context.SaveChanges();
+    }
+
+    public void AddShortenUrl(ShortenUrl shortenUrl)
+    {
+        context.ShortenUrls.Add(shortenUrl);
+
+        context.SaveChanges();
+    }
+
+    public void DeleteShortenUrl(int id)
+    {
+        context.ShortenUrls.Remove(GetShortenUrl(id));
+
+        context.SaveChanges();
+    }
 }
