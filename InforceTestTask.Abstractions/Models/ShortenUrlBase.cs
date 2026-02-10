dbb51d0378cda48855f4b727952f0eb287572929
Michael <muhinmihajlo40@gmail.com>
2026-02-10 16:41:33 +0200

    Added the InforceTestTask.Abstractions project

---
+++ InforceTestTask.Abstractions/Models/ShortenUrlBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InforceTestTask.Abstractions.Models;
+
+public abstract record ShortenUrlBase
+{
+    public Guid Id { get; init; }
+
+    [Required]
+    public required Uri OriginalUrl { get; init; }
+
+    [Required]
+    public required Uri FinalUrl { get; init; }
+
+    [Required]
+    public required DateTime CreatedDate { get; init; }
+
+    [Required]
+    public required Guid UserId { get; init; }
+}
