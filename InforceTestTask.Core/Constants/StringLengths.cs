580789b71759742841eaa147f284d91fd4067d40
Michael <muhinmihajlo40@gmail.com>
2026-02-10 15:02:49 +0200

    Temporary removed the InforceTestTask.Core project

---
+++ InforceTestTask.Core/Constants/StringLengths.cs
@@ -0,0 +1,8 @@
+﻿namespace InforceTestTask.Core.Constants;
+
+public static class StringLengths
+{
+    public const byte UserNameMinLength = 1;
+
+    public const byte PasswordMinLength = 8;
+}
--- InforceTestTask.Core/Models/UserCredentialsBase.cs
+++
@@ -1,8 +0,0 @@
-namespace InforceTestTask.Core.Models;
-
-public abstract record UserCredentialsBase
-{
-    public virtual required string UserName { get; init; }
-
-    public virtual required string Password { get; init; }
-}
